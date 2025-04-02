85f1695cb34f469b57dcdb8b4747b0b1ee21044e
hieunm2 <hieunm2@vnext.vn>
2025-04-02 09:39:59 +0700

    init commit

---
+++ src/main/java/hieu/sagapatternmicroservice/SagaPatternMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package hieu.sagapatternmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagaPatternMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SagaPatternMicroserviceApplication.class, args);
+    }
+
+}
